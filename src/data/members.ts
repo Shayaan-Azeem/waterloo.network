28cd16e1dd310b2b25946dc7fb6b2807d6b7effa
Justin Wu <justinwu137@gmail.com>
2026-02-28 15:00:26 -0500

    justinzwu

--- src/data/members.ts
+++ src/data/members.ts
@@ -31,6 +31,8 @@
  * 3. Set profilePic to: "/photos/your-name.jpg"
  */
 
+import { errorToJSON } from "next/dist/server/render";
+
 export interface Member {
   id: string;
   name: string;
@@ -275,17 +277,17 @@ export const members: Member[] = [
   {
     id: "justin-wu",
     name: "Justin Wu",
-    website: "https://justinwu.net",
+    website: "https://justinzwu.com",
     program: "Systems Design Engineering",
     year: "2030",
     // options: engineering, design, product, growth, ai/ml, research, hardware, quant, software, finance, vc
-    roles: [],
+    roles: ["engineering", "design", "product"],
     // options: fintech, ai, climate, healthcare, edtech, marketplaces, robotics, defense, hard tech, saas, consumer, creator tools
-    verticals: [],
+    verticals: ["ai", "saas", "creator tools"],
     profilePic: "/photos/justin-wu.jpg",
     twitter: "https://x.com/byjustinwu",
-    linkedin: "https://www.linkedin.com/in/justin-wu-171481162/",
-    connections: ["shayaan-azeem", "daniel-ching", "casper-dong", "kevin-thomas", "aayan-rahman"]
+    linkedin: "https://www.linkedin.com/in/justinzijinwu/",
+    connections: ["shayaan-azeem", "daniel-ching", "casper-dong", "kevin-thomas", "aayan-rahman", "raghav-agarwal", "demir-eren", "austin-jian"]
   },
   {
     id: "tony-pan",
